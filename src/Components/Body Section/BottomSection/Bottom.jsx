1e326e09816091291119e4afb1eca6fc16de6dd1
Miguel <viloriamiguel602@gmail.com>
2024-01-23 02:27:51 +0800

    update

--- src/Components/Body Section/BottomSection/Bottom.jsx
+++ src/Components/Body Section/BottomSection/Bottom.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import './bottom.css'
+import "./bottom.css";
 
 const Bottom = () => {
-  return <div>This is Bottom Selection</div>;
+  return (
+    <div className="bottom flex">
+      <div className="graphDiv">
+        <span className="title">All Activities (Graphical View)</span>
+      </div>
+    </div>
+  );
 };
 
 export default Bottom;
-
-export default Bottom
