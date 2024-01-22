cbd3b12cc404fbdde87775f67a8bd3617c8446f7
Miguel <viloriamiguel602@gmail.com>
2024-01-23 02:55:48 +0800

    up

--- src/Components/Body Section/BottomSection/Bottom.jsx
+++ src/Components/Body Section/BottomSection/Bottom.jsx
@@ -1,25 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./bottom.css";
 
-import Chart from "react-apexcharts";
-
 const Bottom = () => {
-  const [state, setState] = useState({
-    options: {
-      colors: ["#01565B", "#5ABA8A"],
-      chart: {
-        id: "basic-bar",
-        tootbar: {
-          show: true,
-        },
-      },
-
-      xaxis: {
-        categories: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
-      },
-    },
-  });
-
   return (
     <div className="bottom flex">
       <div className="graphDiv">
